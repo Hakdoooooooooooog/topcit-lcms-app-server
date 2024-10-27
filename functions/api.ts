a3869045b0d36bc283b55c61cc211a0fdcce223e
Darenz Jasper Hicap <hicap.darenzjasper@gmail.com>
2024-10-27 17:52:49 +0800

    Refactor app.use in api.ts to include base path for Netlify functions

--- functions/api.ts
+++ functions/api.ts
@@ -1,9 +1,8 @@
 // File: /functions/api.ts
 import serverless from "serverless-http";
 import { app } from "../src";
-import { Request, Response, NextFunction } from "express";
 import router from "../src/api/router";
 
-app.use("/", router());
+app.use("/.netlify/functions", router());
 
 export const handler = serverless(app);
