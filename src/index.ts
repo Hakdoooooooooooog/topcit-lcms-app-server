b5e8d2c4f7804d29204e13d35dc18a58a51919db
Darenz Jasper Hicap <hicap.darenzjasper@gmail.com>
2024-10-27 17:30:56 +0800

    Refactor CORS handling in index.ts to include wildcard route

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,8 @@ app.use((req: Request, res: Response, next: NextFunction) => {
       origin: origin,
       credentials: true,
     });
+
+    return next();
   }
 
   res.setHeader("Access-Control-Allow-Credentials", "true");
