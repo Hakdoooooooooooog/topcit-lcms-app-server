1244ce01f0ab5ecea3af3b041a2e25c2171d2d50
Darenz Jasper Hicap <hicap.darenzjasper@gmail.com>
2024-10-27 17:40:34 +0800

    Refactor CORS handling in api.ts to remove unnecessary code

--- functions/api.ts
+++ functions/api.ts
@@ -4,10 +4,6 @@ import { app } from "../src";
 import { Request, Response, NextFunction } from "express";
 import router from "../src/api/router";
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
 app.use("/", router());
 
 export const handler = serverless(app);
