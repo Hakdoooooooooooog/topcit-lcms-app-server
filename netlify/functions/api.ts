02363671d0130708c270383b74ffd8fc062f3c24
Darenz Jasper Hicap <hicap.darenzjasper@gmail.com>
2024-12-07 10:51:32 +0800

    Update environment variables and adjust Netlify configuration; modify Prisma schema for user quiz attempts

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,8 +1,6 @@
 import serverless from "serverless-http";
 import app from "../../src";
 
-if (process.env.NODE_ENV === "production") {
-  module.exports.handler = serverless(app, {
-    basePath: "/api",
-  });
-}
+module.exports.handler = serverless(app, {
+  basePath: "/api",
+});
